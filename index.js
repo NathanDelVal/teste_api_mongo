a362f42508e23b98324608e8191df617779a471d
NathanMad <nathan@internit.com.br>
2025-04-16 08:52:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { MongoClient, Db } = require('mongodb');
+
+const uri = "mongodb+srv://admin:Riot!2007@cluster0.hasqapk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+
+//async await
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db('sample_mflix');
+        const collection = db.collection('movies');
+
+        return {db, collection};
+
+    } catch (e) {
+        console.log(e);
+        // Close the database connection when finished or an error occurs
+        await client.close();
+    }
+}
+
+
+const app = express();
+
+// Define routes before starting the server
+app.get('/', async (req, res) => {
+    connection = await run();
+    const {db, collection} = connection;
+    console.log(await collection.findOne());
+    await client.close();
+});
+
+app.post('/', (req, res) => {
+});
+
+app.put('/', (req, res) => {
+});
+
+app.delete('/', (req, res) => {
+});
+
+// Start server
+app.listen(8080, () => {
+    console.log('🚀 Server running on http://localhost:8080');
+});
